7b1c558cabfdf6d32f9750b22017d61ebc957fcd
leroyloic <loic.mewoulou@cm.maviance.com>
2024-12-09 13:18:36 +0100

    refactor: update module names and add initial implementation for Day 03 tasks

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module main
+module goLearn.com/adventOfCode
 
 go 1.23.4
--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
-	// Call the function to read the input
-	// readInput()cd ..
+	fmt.Println("Hello, World!", Name)
 }
